6fbe7af7e5134a8f7cd00a903634e1e5d7001cf1
Eric Reidelbach <eric.reidelbach@gmail.com>
2022-05-15 14:41:34 -0400

    completed turnover plots

--- 20220513_TurnoverMargin/src/analyze_turnovers.py
+++ 20220513_TurnoverMargin/src/analyze_turnovers.py
@@ -18,9 +18,12 @@ Created on Mon Apr 25 10:00:41 2022
 # Package Import
 #==============================================================================
 import glob
+import matplotlib.pyplot as plt
+import math
 import os  
 import pandas as pd
 import pathlib
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 
 #==============================================================================
 # Reference Variable Declaration
@@ -49,7 +52,8 @@ def renameSchool(df, name_var):
             in the row in the file `school_abbreviations.csv`
     '''  
     # read in school name information
-    df_school_names = pd.read_csv('references/school_abbreviations_and_pictures.csv')
+    df_school_names = pd.read_csv(r'references/school_abbreviations_and_pictures.csv',
+                                  encoding = 'latin-1')
      
     # convert the dataframe to a dictionary such that the keys are the
     #   optional spelling of each school and the value is the standardized
@@ -171,14 +175,193 @@ def loadTurnoverData():
 
     return df_all_years
 
+def importTurnoverFiles():
+    list_files = list(glob.iglob(r'data\*.csv'))
+    df_turnovers = pd.DataFrame()
+    for x in list_files:
+        df_turnovers = df_turnovers.append(pd.read_csv(x))
+    return df_turnovers
+
+def plotStatsWinPct():
+    # Set font and background colour
+    # plt.rcParams.update({'font.family':'Avenir'})
+    bgcol = '#fafafa'
+
+    # Create initial plot
+    fig, ax = plt.subplots(figsize=(6, 4), dpi=1200)
+    ax.scatter(df['Margin'], df['Win_Pct'], s = 10, alpha = 0.5)
+    fig.set_facecolor(bgcol)
+    ax.set_facecolor(bgcol)
+
+    # Change plot spines
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['left'].set_color('#ccc8c8')
+    ax.spines['bottom'].set_color('#ccc8c8')
+
+    # Change ticks
+    plt.tick_params(axis='x', labelsize=12, color='#ccc8c8')
+    plt.tick_params(axis='y', labelsize=12, color='#ccc8c8')
+
+    # Plot badges
+    def getImage(path):
+        return OffsetImage(plt.imread(path), zoom=.3, alpha = 1)
+
+    for index, row in df.iterrows():
+        ab = AnnotationBbox(getImage(row['Logo']), (
+            row['Margin'], row['Win_Pct']), frameon=False)
+        if row['School'] == 'Nebraska':
+            ax.add_artist(ab)
+        
+    # Ensure X-Axis are whole numbers
+    def round_down(m, n):
+        return m // n * n
+    def round_up(n):
+        return n + (5 - n) % 5
+    min_xaxis = round_down(math.floor(df['Margin'].min()),5)
+    max_xaxis = round_up(math.ceil(df['Margin'].max()))
+    # new_xaxis = range(min_xaxis, max_xaxis+5, 5)
+    # ax.set_xticks(list(new_xaxis))
+
+    # Set Y-Axis to traditional height values (i.e. 6'1" etc)
+    min_yaxis = math.floor(df['Win_Pct'].min())
+    max_yaxis = math.ceil(df['Win_Pct'].max())
+    new_yaxis = range(min_yaxis, max_yaxis+1)
+    new_yaxis = list(range(0, 10, 1))
+    new_yaxis = [x/10 for x in new_yaxis]
+                      
+    # ax.set_yticks(list(new_yaxis))
+    # yaxis_labels = [str(x) for x in new_yaxis]
+    # yaxis_labels = [str(math.floor(int(y)/12)) + '-' + str(int(y)%12) for y in yaxis_labels]
+    # ax.set_yticklabels(yaxis_labels)
+
+    # Add average lines
+    plt.hlines(df['Win_Pct'].median(), 
+               min_xaxis, 
+               max_xaxis, 
+               color='#c2c1c0')
+    plt.vlines(df['Margin'].median(), 
+               min_yaxis, 
+               max_yaxis, 
+               color='#c2c1c0')
+         
+    fig.text(.15,.98,'Turnovers in FBS 2012-2021',size=20)
+    fig.text(.15,.93,'Winning Pct. vs. Turnover Margin', size=12)
+
+    # Set Axis Labels
+    ax.set_xlabel('Tunover Margin.')
+    ax.set_ylabel('Win Pct.')
+
+    # Add Made by to bottom of image
+    fig.text(.68, .02, 'Created by @Stewmanji', size=8, color='#c2c1c0')
+
+    ## Save plot
+    plt.savefig(r'images/plots/turnover_stats_win_pct.png', dpi=1200, bbox_inches = "tight")
+    plt.savefig(r'images/plots/turnover_stats_win_pct.pdf', format = 'pdf', bbox_inches = "tight")
+
+def plotStatsMargin():
+    # Set font and background colour
+    # plt.rcParams.update({'font.family':'Avenir'})
+    bgcol = '#fafafa'
+
+    # Create initial plot
+    fig, ax = plt.subplots(figsize=(6, 4), dpi=1200)
+    ax.scatter(df['TO'], df['Opp_TO'], s = 10, alpha = 0.5)
+    fig.set_facecolor(bgcol)
+    ax.set_facecolor(bgcol)
+
+    # Change plot spines
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['left'].set_color('#ccc8c8')
+    ax.spines['bottom'].set_color('#ccc8c8')
+
+    # Change ticks
+    plt.tick_params(axis='x', labelsize=12, color='#ccc8c8')
+    plt.tick_params(axis='y', labelsize=12, color='#ccc8c8')
+
+    # Plot badges
+    def getImage(path):
+        return OffsetImage(plt.imread(path), zoom=.3, alpha = 1)
+
+    for index, row in df.iterrows():
+        ab = AnnotationBbox(getImage(row['Logo']), (
+            row['TO'], row['Opp_TO']), frameon=False)
+        if row['School'] == 'Nebraska':
+            ax.add_artist(ab)
+        
+    # Ensure X-Axis are whole numbers
+    def round_down(m, n):
+        return m // n * n
+    def round_up(n):
+        return n + (5 - n) % 5
+    min_xaxis = round_down(math.floor(df['TO'].min()),5)
+    max_xaxis = round_up(math.ceil(df['TO'].max()))
+    # new_xaxis = range(min_xaxis, max_xaxis+5, 5)
+    # ax.set_xticks(list(new_xaxis))
+
+    # Set Y-Axis to traditional height values (i.e. 6'1" etc)
+    min_yaxis = math.floor(df['Opp_TO'].min())
+    max_yaxis = math.ceil(df['Opp_TO'].max())
+    new_yaxis = range(min_yaxis, max_yaxis+1)
+    new_yaxis = list(range(0, 10, 1))
+    new_yaxis = [x/10 for x in new_yaxis]
+                      
+    # ax.set_yticks(list(new_yaxis))
+    # yaxis_labels = [str(x) for x in new_yaxis]
+    # yaxis_labels = [str(math.floor(int(y)/12)) + '-' + str(int(y)%12) for y in yaxis_labels]
+    # ax.set_yticklabels(yaxis_labels)
+
+    # Add average lines
+    plt.hlines(df['TO'].median(), 
+               min_xaxis, 
+               max_xaxis, 
+               color='#c2c1c0')
+    plt.vlines(df['Opp_TO'].median(), 
+               min_yaxis, 
+               max_yaxis, 
+               color='#c2c1c0')
+         
+    fig.text(.15,.98,'Turnovers in FBS 2012-2021',size=20)
+    fig.text(.15,.93,'Turnovers Gained vs. Turnovers Lost', size=12)
+
+    # Set Axis Labels
+    ax.set_xlabel('Turnovers')
+    ax.set_ylabel('Opponent Turnovers')
+
+    # Add Made by to bottom of image
+    fig.text(.68, .02, 'Created by @Stewmanji', size=8, color='#c2c1c0')
+
+    ## Save plot
+    plt.savefig(r'images/plots/turnover_stats_margin.png', dpi=1200, bbox_inches = "tight")
+    plt.savefig(r'images/plots/turnover_stats_margin.pdf', format = 'pdf', bbox_inches = "tight")
+
 #==============================================================================
 # Working Code
 #==============================================================================
 
 # Set the project working directory
-path_dir = pathlib.Path(
-    r'C:\Users\reideej1\Projects\a_Personal\huskerProjects\20220513_TurnoverMargin')
+path_dir = pathlib.Path(r'huskerProjects\20220513_TurnoverMargin')
 os.chdir(path_dir)
 
 # ingest turnvoer data for all available years
 df_turnovers = loadTurnoverData()
+
+df_turnovers = importTurnoverFiles()
+df_turnovers['Win_Pct'] = df_turnovers['W']/df_turnovers['G']
+df_turnovers['Margin'] = df_turnovers['Opp_TO'] - df_turnovers['TO']
+df = df_turnovers[['Year', 'School', 'Conf', 'Rank', 'G', 'W', 'L', 'Win_Pct', 
+                   'Opp_Fum', 'Opp_Int', 'Opp_TO', 'Fum', 'Int', 'TO', 
+                   'Margin/G', 'Margin']].copy()
+# Add Logos to table
+list_image_paths = []
+for school in df['School']:
+    list_image_paths.append(rf'images/logos_school_square/{school}.png')
+df['Logo'] = list_image_paths
+
+# plot turnover stats
+plotStatsWinPct()
+plotStatsMargin()
+
+# what is the avg margin of each 64th placed team
+df64 = df[df['Rank'] == 64]
